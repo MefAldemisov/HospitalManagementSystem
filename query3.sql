639f04187ed4df3f00449f77efa44d0929a37f74
Kazybek Askarbek <k.askarbek@innopolis.university>
2019-11-27 22:20:54 +0300

    Update query3.sql

--- query3.sql
+++ query3.sql
@@ -1,10 +1,10 @@
-SELECT tits2.medical_insurence_number FROM
-	(SELECT tits.medical_insurence_number, tits.count, tits.week FROM (SELECT COUNT(medical_insurence_number) as count,medical_insurence_number, date_trunc('week', appointment_timestamp) as week
+SELECT select2.medical_insurence_number FROM
+	(SELECT select1.medical_insurence_number, select1.count, select1.week FROM (SELECT COUNT(medical_insurence_number) as count,medical_insurence_number, date_trunc('week', appointment_timestamp) as week
 		FROM createappointment 
 		WHERE appointment_timestamp > date_trunc('week', current_date) - interval '3 week' AND appointment_timestamp <= current_date
-		GROUP BY week, medical_insurence_number) as tits
-		GROUP BY tits.medical_insurence_number, tits.count, tits.week
-		HAVING tits.count >=2
-	) as tits2
-GROUP BY tits2.medical_insurence_number
-HAVING  COUNT(tits2.medical_insurence_number) > 3
+		GROUP BY week, medical_insurence_number) as select1
+		GROUP BY select1.medical_insurence_number, select1.count, select1.week
+		HAVING select1.count >=2
+	) as select2
+GROUP BY select2.medical_insurence_number
+HAVING  COUNT(select2.medical_insurence_number) > 3
