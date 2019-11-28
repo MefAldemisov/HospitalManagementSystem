c561d7f3749d1b9a4fc176ad8be03333e6930cde
MefAldemisov <alisha-omg@yandex.ru>
2019-11-28 15:07:38 +0300

    Extra files removed

--- additionalInserts.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO CreateAppointment(appointment_timestamp, year, month, medical_insurence_number, doctor_SSN, room)
-VALUES ('2019-11-25 15:25:18', '1990', 1, 2576561477010630, '506623239', 163);
