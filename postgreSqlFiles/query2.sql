fde6195645c9301a5ddcc3e33194017a5c037f0e
MefAldemisov <alisha-omg@yandex.ru>
2019-11-28 17:16:51 +0300

    BD updated + 2-nd query

--- postgreSqlFiles/query2.sql
+++ postgreSqlFiles/query2.sql
@@ -2,12 +2,10 @@ SELECT timeslot, COUNT(week) AS total, (COUNT(week)/52.1429) AS average
 FROM (	SELECT EXTRACT(WEEK FROM appointment_timestamp) AS week,
  		CAST(EXTRACT(ISODOW FROM appointment_timestamp) AS TEXT) ||
  		' ' ||
- 		CAST(EXTRACT(HOUR FROM appointment_timestamp) AS TEXT) ||
- 		':' ||
- 		CAST(EXTRACT(MINUTE FROM appointment_timestamp) AS TEXT) AS timeslot
+ 		SUBSTRING(CAST(appointment_timestamp AS TEXT), 12, 5) AS timeslot
 		FROM (	SELECT appointment_timestamp
 				FROM createappointment
-				WHERE appointment_timestamp > '2018-11-28 00:00:00'
+WHERE appointment_timestamp > '2018-11-28 00:00:00'
 			 ) as timestamp_during_year
 		GROUP BY appointment_timestamp
 		ORDER BY week
